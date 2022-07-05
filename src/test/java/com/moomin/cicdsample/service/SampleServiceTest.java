5ea5c85b2183b863fb73b8720c84a3c962f92bdc
Moomin <moominK228@gmail.com>
2022-07-05 18:14:48 +0900

    안쓰는 import 하나 지움

--- src/test/java/com/moomin/cicdsample/service/SampleServiceTest.java
+++ src/test/java/com/moomin/cicdsample/service/SampleServiceTest.java
@@ -1,6 +1,5 @@
 package com.moomin.cicdsample.service;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
