b0c137ca06b53b7a7e5b5d4a27014e0f59a5f297
Moomin <moominK228@gmail.com>
2022-07-05 17:13:28 +0900

    ci-cd 실습을 위한 maven 프로젝트 생성

---
+++ ci-cd-sample/src/test/java/com/moomin/cicdsample/CiCdSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moomin.cicdsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
